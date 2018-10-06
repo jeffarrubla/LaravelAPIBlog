efe92c9d87e71c2b8df8e03f12dbf9b01798dd7f
Jeff <none>
2018-10-06 21:24:48 +0000

    Added registration

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::get('articles/{article}', 'ArticleController@show');
 Route::post('articles', 'ArticleController@store');
 Route::put('articles/{article}', 'ArticleController@update');
 Route::delete('articles/{article}', 'ArticleController@delete');
+Route::post('register', 'Auth\RegisterController@register');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
